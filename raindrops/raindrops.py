b30ea5d3a3d6df955f4f8cbc6e61d6e4793226a9
Nefer Blanchar <nblancha1@cuc.edu.co>
2021-10-02 13:51:09 -0500

    ejercicio 2

---
+++ raindrops/raindrops.py
@@ -0,0 +1,12 @@
+def convert(num):
+    resultado = ''
+    if (num % 3 == 0):
+        resultado = 'Pling'
+    if (num % 5 == 0):
+        resultado = resultado + 'Plang'
+    if (num % 7 == 0):
+        resultado = resultado + 'Plong'
+    if (resultado == ''):
+        resultado = str(num)
+    return resultado
+    
